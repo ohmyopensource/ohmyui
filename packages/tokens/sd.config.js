4c11e9c2c8c7116cb8b8700a68e4229357951d4e
Luca D'Aurizio <daurizio.lc@gmail.com>
2026-08-11 12:07:16 +0200

    feat: update color, spacing and typography tokens, extend Style Dictionary config

--- packages/tokens/sd.config.js
+++ packages/tokens/sd.config.js
@@ -1,3 +1,6 @@
+import { copyFileSync, mkdirSync } from 'fs';
+import { join } from 'path';
+
 export default {
   source: ['src/**/*.json'],
   platforms: {
@@ -20,6 +23,26 @@ export default {
           format: 'flutter/class.dart',
         },
       ],
+    },
+  },
+  hooks: {
+    'build:end': () => {
+      const outDir = 'build/css/fonts';
+      mkdirSync(outDir, { recursive: true });
+
+      const files = [
+        'Outfit-VariableFont_wght.woff2',
+        'Outfit-VariableFont_wght.ttf',
+        'Quicksand-VariableFont_wght.woff2',
+        'Quicksand-VariableFont_wght.ttf',
+        'fonts.css',
+      ];
+
+      for (const f of files) {
+        copyFileSync(join('src/fonts', f), join(outDir, f));
+      }
+
+      console.log('✓ fonts copied to build/css/fonts/');
     },
   },
 };
